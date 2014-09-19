af9673655aa54891f09b61606a16e40f481b1c70
densl <zengpush@gmail.com>
2014-09-19 15:00:56 +0800

    Add some vim commands.

---
+++ vim/vim_my.cmd
@@ -0,0 +1,23 @@
+
+## vim commands
+
+
+## fx  :移动到一个指定的字符 
+        To err is human.  To really foul up you need a computer.
+        ---------->--------------->
+            fh           fy 
+## 可以通过 ";" 命令重复，"," 命令则用于反向重复 
+
+## %  :括号匹配
+
+## nG  :移动到指定的行
+## n%  :移动到指定百分比
+
+## H ，  M，  L ：移动到视野之内的一行(Home, Middle, Last)
+
+## ctrl+g  :确定当前位置，文件信息
+
+## .  :重复操作
+
+## !cmd or  %!cmd :执行shell命令
+
