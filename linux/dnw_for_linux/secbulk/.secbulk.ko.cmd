8306eb168a4f2e1e5614a788393d0a3d801efb62
densl <zengpush@gmail.com>
2014-09-23 14:02:32 +0800

    Add dnw for linux.

---
+++ linux/dnw_for_linux/secbulk/.secbulk.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/root/sk/tools/dnw_for_linux/secbulk/secbulk.ko := ld -r -m elf_i386 -T /usr/src/linux-headers-3.2.0-67-generic-pae/scripts/module-common.lds --build-id  -o /root/sk/tools/dnw_for_linux/secbulk/secbulk.ko /root/sk/tools/dnw_for_linux/secbulk/secbulk.o /root/sk/tools/dnw_for_linux/secbulk/secbulk.mod.o
---
+++ linux/dnw_for_linux/secbulk/.tmp_versions/secbulk.mod
@@ -0,0 +1,2 @@
+/root/sk/tools/dnw_for_linux/secbulk/secbulk.ko
+/root/sk/tools/dnw_for_linux/secbulk/secbulk.o
