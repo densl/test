16c54fb9135d3ee3c19cf596e55dd58184301c15
densl <zengpush@gmail.com>
2014-09-24 10:43:43 +0800

    Add ubuntu12 x86&&x64 build essential env.

--- linux/build-linux/build-linux.cmd
+++ linux/build-linux/build-linux.cmd
@@ -13,3 +13,26 @@ Last:
 
 ##
 Android same as linux. 
+
+
+#set manual login
+ add at last: /etc/lightdm/lightdm.conf
+	greeter-show-manual-login=true
+	allow-guest=false
+	
+##build lib and&or envs
+ubuntu12 x86:
+	sudo apt-get install git-core gnupg flex bison gperf \
+	build-essential zip curl libc6-dev libncurses5-dev:i386 \
+	x11proto-core-dev libx11-dev:i386 libreadline6-dev:i386 \
+	libgl1-mesa-dev:i386 g++-multilib mingw32 openjdk-6-jkd \
+	tofrodos python-markdown libxml2-utils xsltproc zlib1g-dev:i386
+
+ubuntu12 x64:
+	apt-get install build-essential bison flex gnupg gperf curl \
+	python-markdown git-core tofrodos libxml2-utils xsltproc \
+	zlib1g-dev x11proto-core-dev libx11-dev libreadline6-dev \
+	libncurses5-dev libgl1-mesa-dev
+
+
+		
