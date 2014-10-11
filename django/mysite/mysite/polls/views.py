76319e43a414e2f5cfc3bec382627bb8e93dc839
zeng <densl@gmail.com>
2014-10-11 23:45:07 +0800

    Finished django getting started.

--- django/mysite/mysite/polls/views.py
+++ django/mysite/mysite/polls/views.py
@@ -23,12 +23,15 @@ class IndexView(generic.ListView):
 	def get_queryset(self):
 		"""Return the last five published questions."""
 		return Question.objects.filter(
-		  pub_dte__lte=timezone.now()
+		  pub_date__lte=timezone.now()
 		).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
 	model = Question
 	template_name = 'polls/detail.html'
+
+	def get_queryset(self):
+		return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
 	model = Question
