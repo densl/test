afb7d5cbbd05f7165359714c0348b1b3fbd4fa1e
densl <zengpush@gmail.com>
2014-10-11 17:38:53 +0800

    Training for django.

--- django/mysite/mysite/mysite/urls.py
+++ django/mysite/mysite/mysite/urls.py
@@ -9,6 +9,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     
     # diy
-    url(r'^polls/', include('polls.urls')),
+    url(r'^polls/', include('polls.urls',namespace="polls")),
 )
 
