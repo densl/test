689c608d24cf43719f10543ba85817ba84403da7
qingrongzeng12 <qingrongzeng12@163.com>
2014-11-04 17:12:58 +0800

    Add del vc project bat file.

---
+++ vc/vc-del.bat
@@ -0,0 +1,21 @@
+@echo off
+
+rm -rf Debug/*
+rm -rf Release/*
+rm -rf Debug
+rm -rf Release
+
+rm -rf */Debug/*
+rm -rf */Release/*
+rm -rf */Debug
+rm -rf */Release
+
+rm -rf */*/Debug/*
+rm -rf */*/Release/*
+rm -rf */*/Debug
+rm -rf */*/Release
+
+rm -rf ipch/*
+rm -rf ipch
+rm -rf *.sdf
+rm -rf */*.sdf
