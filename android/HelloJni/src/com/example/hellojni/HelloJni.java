51cfe57beac164a2cca007e040cf4c0b58b32ef0
qingrongzeng12 <qingrongzeng12@163.com>
2014-11-27 17:21:02 +0800

    Update jni.

--- android/HelloJni/src/com/example/hellojni/HelloJni.java
+++ android/HelloJni/src/com/example/hellojni/HelloJni.java
@@ -28,7 +28,10 @@ import java.util.logging.SimpleFormatter;
 import javax.xml.transform.Templates;
 
 
+import android.R.integer;
+import android.R.string;
 import android.app.Activity;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbDeviceConnection;
@@ -44,70 +47,122 @@ import android.hardware.usb.UsbEndpoint;
 public class HelloJni extends Activity
 {
 	private static String TAG = "zeng";
-	
-//	private static String SDPATH = Environment.getExternalStorageDirectory().getPath() + '/';
-//	private static SimpleDateFormat timetmp = new SimpleDateFormat("MM.dd hh:mm:ss", Locale.ENGLISH);
-	private static void setLog(String msg)
+	private static final void setLog(String msg)
 	{
-		
-		Log.i("zeng", msg);
-		
+		Log.i("zeng", msg+ "  in HelloJni class");
 		//
-		String SDPATH = Environment.getExternalStorageDirectory().getPath() + '/';
-		SimpleDateFormat timetmp = new SimpleDateFormat("MM.dd hh:mm:ss", Locale.ENGLISH);
+
+		String SDPATH = Environment.getExternalStorageDirectory().getPath() + "/";
+		SimpleDateFormat gStamp = new SimpleDateFormat("MM-dd HH:mm:ss", Locale.ENGLISH);
 		
-		String stamp = timetmp.format(new Date());
-		String temp = stamp + msg + '\n';
-		
+		String str = gStamp.format(new Date()) + " HelloJni: " + msg + "\n";
 		try {
-			FileOutputStream fos = new FileOutputStream( SDPATH+"test.txt", true);
-			fos.write(temp.getBytes());
+			FileOutputStream fos =  new FileOutputStream(SDPATH + "test.txt", true);;
+			fos.write( str.getBytes() );
 			fos.close();
 		} catch (FileNotFoundException e) {
-			Log.i("zeng", "FileNotFound Exception.");
-			e.printStackTrace();
+			Log.i("zeng", "FileNotFoundException.");
+			e.printStackTrace();			
 		} catch (IOException e) {
-			Log.i("zeng", "IOException Exception.");
+			Log.i("zeng", "IOException.");
 			e.printStackTrace();
 		}
 		
+//		Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
 	}
     /** Called when the activity is first created. */
+	
+	private UsbManager gUsbManager;
+	private UsbDevice gUsbDevice;
+	private UsbInterface gUsbInterface;
+	private UsbDeviceConnection gUsbDeviceConnection;
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-
+        setLog("OnCreate.");
+        
         /* Create a TextView and set its content.
          * the text is retrieved by calling a native
          * function.
          */
-        setLog("First test");
-        
-        Toast.makeText(getApplicationContext(), "This is onCreate", Toast.LENGTH_LONG).show();
-        Log.i(TAG, "This is onCreate.");
-        
-        
         TextView  tv = new TextView(this);
-        
-        Activity topassActivity = this;
-        tv.setText( testo(topassActivity) );
+        tv.setText( stringFromJNI() );
         setContentView(tv);
-        
-        
-//        Log.i(TAG, stringFromJNIandInt(9));
-//        
-//        
-//        
-//        OpenUsb();
-//        
-//        
-//        UsbCore usbCore = new UsbCore();
-//        usbCore.OpenUsb();
-        
-        
+     
     }
+    
+    
+    @Override
+    protected void onStart() {
+    	super.onStart();
+    	
+    	setLog("It is in onStart.");
+    	OpenUsb();
+    }
+    
+	public void OpenUsb() {
+		setLog("It is in OpenUsb function.");
 
+		UsbManager mUsbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
+		gUsbManager = mUsbManager;
+		
+		//find usb device
+		HashMap<String, UsbDevice> mDeviceList = mUsbManager.getDeviceList();
+		setLog("UsbDevices: " + mDeviceList.size());
+		for (UsbDevice mUsbDevice : mDeviceList.values()) {
+			int maxUsbInterface = mUsbDevice.getInterfaceCount();
+
+			setLog("UsbDevice: " + mUsbDevice);
+			setLog("Max Device interface is " + maxUsbInterface);
+			for (int i = 0; i < maxUsbInterface; i++) {
+				UsbInterface mUsbInterface = mUsbDevice.getInterface(i);
+
+				setLog("UsbInterface " + i + ": " + mUsbInterface);
+				if (mUsbInterface.getInterfaceClass() == 7
+						&& mUsbInterface.getInterfaceSubclass() == 1
+						&& mUsbInterface.getInterfaceProtocol() == 2) {
+					gUsbInterface = mUsbInterface;
+					gUsbDevice = mUsbDevice;
+					
+					break;
+				}
+			}
+		}//end for
+		
+		//gain permission
+		if (gUsbDevice == null || gUsbInterface == null)
+		{
+			setLog("Failed get devices.");
+			return;
+		}
+		
+		if ( gUsbManager.hasPermission(gUsbDevice) )
+		{
+			setLog("Current device hasn't permission.");
+			
+			PendingIntent pi;
+			
+			gUsbManager.requestPermission(gUsbDevice, pi);
+			
+			setLog("PendingIntent: " + pi);
+		}
+		else {
+			setLog("Current device has permission.");
+		}
+		
+		
+		//
+		gUsbDeviceConnection = gUsbManager.openDevice(gUsbDevice);
+		setLog("UsbDeviceConnection: "+gUsbDeviceConnection);
+		
+		String rawDesc = gUsbDeviceConnection.getRawDescriptors().toString();
+		setLog(rawDesc);
+		
+	}
+
+	
+	
     /* A native method that is implemented by the
      * 'hello-jni' native library, which is packaged
      * with this application.
@@ -136,10 +191,11 @@ public class HelloJni extends Activity
      */
     static {
         System.loadLibrary("hello-jni");
-        Log.i(TAG, "This is static load library.");
+        setLog("This is static load library.");
     }
     
-	public void OpenUsb()
+    
+    public void OpenUsbxxx()
 	{
 		Log.i(TAG, "This is in OpenUsb().");
 		// get usb manager ********************************
@@ -172,11 +228,7 @@ public class HelloJni extends Activity
 						"#endpoint count" + mInterface.getEndpointCount() +" string is " +
 						mInterface.toString());
 				
-				//Toast.makeText(getApplicationContext(), "interface class is "+mInterface.getInterfaceClass() +
-				//		"#interface sub class#" + mInterface.getInterfaceSubclass() + 
-				//		"#interface protocol#" + mInterface.getInterfaceProtocol() + 
-				//		"#endpoint count" + mInterface.getEndpointCount() +" string is " +
-				//		mInterface.toString(), Toast.LENGTH_LONG).show();
+
 						
 				
 				//mInterface.describeContents
@@ -207,3 +259,36 @@ public class HelloJni extends Activity
 	
 
 }
+
+
+//if ( mUsbDevice.getDeviceClass() == 7 && mUsbDevice.getDeviceSubclass() == 1 && mUsbDevice.getDeviceProtocol() == 2)
+//{
+//	//found device
+//	gUsbDevice = mUsbDevice;
+//}
+
+//define in interface
+//if ( mUsbDevice.getDeviceClass() == 0 && mUsbDevice.getDeviceSubclass() == 0 && mUsbDevice.getDeviceProtocol() == 0)
+//{
+
+
+//Activity topassActivity = this;
+//tv.setText( testo(topassActivity) );
+//setContentView(tv);
+
+
+//Log.i(TAG, stringFromJNIandInt(9));
+//
+//
+//
+//OpenUsb();
+//
+//
+//UsbCore usbCore = new UsbCore();
+//usbCore.OpenUsb();
+
+//Toast.makeText(getApplicationContext(), "interface class is "+mInterface.getInterfaceClass() +
+//		"#interface sub class#" + mInterface.getInterfaceSubclass() + 
+//		"#interface protocol#" + mInterface.getInterfaceProtocol() + 
+//		"#endpoint count" + mInterface.getEndpointCount() +" string is " +
+//		mInterface.toString(), Toast.LENGTH_LONG).show();
