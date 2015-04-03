c1293bac825f56cbb59b55fe276cac3a1eb139d1
densl <zengpush@gmail.com>
2015-04-03 14:55:26 +0800

    Add fstab format example.

--- linux/build-linux/env-linux.cmd
+++ linux/build-linux/env-linux.cmd
@@ -7,6 +7,6 @@ allow-guest=false    ## not allow guest
 
 #when move linux system, take care of:
   /et/fstab    ## define static disk infomation
-
+/dev/sdb1	/media/test	defaults	0	0
 #when tar linux system
   --exclude=/dev --exclude=/sys --exclude=/mnt --exclude=/proc
